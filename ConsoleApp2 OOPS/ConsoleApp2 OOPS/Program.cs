ae79c6f6cb789c6dcbca097558bcb74a1eed583a
Priyam Shukla <priyam.shukla@unthinkable.co>
2021-12-11 00:02:31 +0530

    OOPS

---
+++ ConsoleApp2 OOPS/ConsoleApp2 OOPS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2_OOPS
+{
+    class Program
+    {
+        String name = "Abc";
+        int roll = 12345;
+        int marks = 20;
+        int sub1_marks;
+        int sub2_marks;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.name);
+            Console.WriteLine(p.roll);
+            Console.WriteLine(p.marks);
+            p.sub1_marks = 20;
+            p.sub2_marks = 23;
+            Console.WriteLine(p.sub1_marks);
+            Console.WriteLine(p.sub2_marks);
+            Console.ReadLine();
+        }
+    }
+}
