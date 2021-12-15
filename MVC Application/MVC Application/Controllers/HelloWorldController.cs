d790113e28c5e66a835413f963e9dbc38ff3a556
Techcoder5 <priyam.shukla@unthinkable.co>
2021-12-15 12:00:48 +0530

    Creating First Controller

---
+++ MVC Application/MVC Application/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "Welcome to MVC First Controller Creation";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "How's the experience of creatimg the first MVC Application";
+        }
+    }
+}
