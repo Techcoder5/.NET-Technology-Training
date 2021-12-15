c37a8f3ed902dd2042d2493b7af55f0f2f5272e5
Techcoder5 <priyam.shukla@unthinkable.co>
2021-12-15 14:27:18 +0530

    Adding Controllers and calling Action Methods from Browser

---
+++ MVC APPLICATION CONTROLLERS/MVC APPLICATION CONTROLLERS/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_APPLICATION_CONTROLLERS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Welcome to New MVC Controller";
+        }
+        public string share_feedback()
+        {
+            return "Share your Feedback";
+        }
+    }
+}
