96dc4e4b219eacdb3f23ad30cfb2e8fa87041829
Techcoder5 <priyam.shukla@unthinkable.co>
2021-12-15 17:14:55 +0530

    Calling Action Action Methods using parameters from browser

--- MVC/MVC APPLICATION CONTROLLERS/MVC APPLICATION CONTROLLERS/Controllers/HomeController.cs
+++ MVC/MVC APPLICATION CONTROLLERS/MVC APPLICATION CONTROLLERS/Controllers/HomeController.cs
@@ -9,13 +9,14 @@ namespace MVC_APPLICATION_CONTROLLERS.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+
         public string Index()
         {
             return "Welcome to New MVC Controller";
         }
-        public string share_feedback()
+        public string share_feedback(int id)
         {
-            return "Share your Feedback";
+            return "Share your Feedback"+ id;
         }
     }
 }
